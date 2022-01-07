3eef1fb8f5f07db7bb794f89e41ceda51bfc87b1
Tyler-Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2022-01-07 12:05:51 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/stravaart/MainActivity.kt
+++ app/src/main/java/com/example/stravaart/MainActivity.kt
@@ -1,43 +1,18 @@
 package com.example.stravaart
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.AttributeSet
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.stravaart.ui.theme.StravaArtTheme
-import com.sweetzpot.stravazpot.authenticaton.api.AccessScope
-import com.sweetzpot.stravazpot.authenticaton.api.ApprovalPrompt
-import com.sweetzpot.stravazpot.authenticaton.api.StravaLogin
-import com.sweetzpot.stravazpot.authenticaton.ui.StravaLoginButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StravaArtTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Row(modifier = Modifier.fillMaxSize()) {
-                        val intent: Intent = StravaLogin
-                            .withContext(this@MainActivity)
-                            .withClientID(5)
-                            .withRedirectURI("test")
-                            .withApprovalPrompt(ApprovalPrompt.AUTO)
-                            .withAccessScope(AccessScope.VIEW_PRIVATE_WRITE)
-                            .makeIntent()
-                        startActivity(intent)
-                        Text("hello")
-                    }
-                }
             }
         }
     }
