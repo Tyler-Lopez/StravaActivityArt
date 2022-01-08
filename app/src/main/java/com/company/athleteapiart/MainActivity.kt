26a5e9f3a2a725aa7732f5e5988fc78c1944f9ce
BunsenBurnsRust <77797048+BunsenBurnsRust@users.noreply.github.com>
2022-01-08 17:30:54 -0500

    oauth2 returning to app

--- app/src/main/java/com/company/athleteapiart/MainActivity.kt
+++ app/src/main/java/com/company/athleteapiart/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.company.athleteapiart
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,13 +14,27 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.company.athleteapiart.ui.theme.AthleteApiArtTheme
 
 class MainActivity : ComponentActivity() {
+    val intentUri = Uri.parse("https://www.strava.com/oauth/mobile/authorize")
+        .buildUpon()
+        .appendQueryParameter("client_id", "75992")
+        .appendQueryParameter("redirect_uri", "com.company.athleteapiart://myapp.com")
+        .appendQueryParameter("response_type", "code")
+        .appendQueryParameter("approval_prompt", "auto")
+        .appendQueryParameter("scope", "activity:read_all")
+        .build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AthleteApiArtTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Button(onClick = {
+                        val intent = Intent(Intent.ACTION_VIEW, intentUri)
+                        startActivity(intent)
+                    }) {
+                        Text("Login with Strava")
+                    }
                 }
             }
         }
