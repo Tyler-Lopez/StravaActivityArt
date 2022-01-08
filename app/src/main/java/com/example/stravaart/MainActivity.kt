2e3f22cf2fb63e3ff4af32a268d909f528428a85
BunsenBurnsRust <77797048+BunsenBurnsRust@users.noreply.github.com>
2022-01-08 00:00:36 -0500

    OAUTH2

--- app/src/main/java/com/example/stravaart/MainActivity.kt
+++ app/src/main/java/com/example/stravaart/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.stravaart
 
+import android.accounts.Account
+import android.accounts.AccountManager
+import android.accounts.AccountManagerCallback
+import android.accounts.AccountManagerFuture
+import android.media.session.MediaSessionManager
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
@@ -9,10 +15,29 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.stravaart.ui.theme.StravaArtTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+    // https://developer.android.com/training/id-auth/authenticate
+        val am: AccountManager = AccountManager.get(this)
+        val options = Bundle()
+
+        val onTokenAcquired = AccountManagerCallback<Bundle> {
+            TODO("Not yet implemented")
+        }
+
+        am.getAuthToken(
+            Account("test", "test"),
+           "read_all",
+            options,
+            this,
+            onTokenAcquired,
+            Handler()
+        )
+
         super.onCreate(savedInstanceState)
         setContent {
             StravaArtTheme {
+
             }
         }
     }
