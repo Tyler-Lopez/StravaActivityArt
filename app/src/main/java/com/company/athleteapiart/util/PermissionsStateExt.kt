af7187b47f01b7f2afe9146703b7d273cb0b8c86
BunsenBurnsRust <77797048+BunsenBurnsRust@users.noreply.github.com>
2022-01-12 22:52:28 -0500

    Update PermissionsStateExt.kt

--- app/src/main/java/com/company/athleteapiart/util/PermissionsStateExt.kt
+++ app/src/main/java/com/company/athleteapiart/util/PermissionsStateExt.kt
@@ -3,6 +3,8 @@ package com.company.athleteapiart.util
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionState
 
+// https://www.youtube.com/watch?v=ltHN50BdDc4
+// How to Do PROPER Permission Handling in Jetpack Compose - Android Studio Tutorial
 @ExperimentalPermissionsApi
 fun PermissionState.isPermaDenied(): Boolean {
     return !shouldShowRationale && !hasPermission
