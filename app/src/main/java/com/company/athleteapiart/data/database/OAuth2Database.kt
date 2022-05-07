df4b0e55cd6bd8eff7ba2fa289e8f6f69fd87c26
Tyler-Lopez <tylerlopezmi@gmail.com>
2022-05-06 22:22:38 -0400

    Spacing

--- app/src/main/java/com/company/athleteapiart/data/database/OAuth2Database.kt
+++ app/src/main/java/com/company/athleteapiart/data/database/OAuth2Database.kt
@@ -14,6 +14,7 @@ import com.company.athleteapiart.data.entities.OAuth2Entity
     version = 1,
     exportSchema = false
 )
+
 abstract class OAuth2Database : RoomDatabase() {
 
     abstract val oAuth2Dao: OAuth2Dao
--- app/src/main/java/com/company/athleteapiart/data/database/UserDatabase.kt
+++ app/src/main/java/com/company/athleteapiart/data/database/UserDatabase.kt
@@ -15,6 +15,7 @@ import com.company.athleteapiart.data.entities.UserEntity
     version = 1,
     exportSchema = false
 )
+
 abstract class UserDatabase : RoomDatabase() {
 
     abstract val userDao: UserDao
