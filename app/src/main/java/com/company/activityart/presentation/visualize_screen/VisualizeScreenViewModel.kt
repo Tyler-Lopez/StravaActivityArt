5890ecb2658d8f6bf4b1378c26f2efe435116cfd
Tyler-Lopez <tylerlopezmi@gmail.com>
2022-06-08 19:27:07 -0400

    Update VisualizeScreenViewModel.kt

--- app/src/main/java/com/company/activityart/presentation/visualize_screen/VisualizeScreenViewModel.kt
+++ app/src/main/java/com/company/activityart/presentation/visualize_screen/VisualizeScreenViewModel.kt
@@ -254,6 +254,7 @@ class VisualizeScreenViewModel @Inject constructor(
 
         return _activities.mapIndexed { index, act ->
             // Decode each Polyline into a List<LatLng>
+            println("Here summary polyline was ${act.summaryPolyline}")
             PolyUtil.decode(act.summaryPolyline).let { latLngList ->
 
                 // Convert List<LatLng> to List<Pair<Float, Float>>
