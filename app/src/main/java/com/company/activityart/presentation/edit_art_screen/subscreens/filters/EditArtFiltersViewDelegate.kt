95b813e9d82ba257fca1e821958695cb7ca96250
Tyler Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2022-09-14 22:33:01 -0400

    Update EditArtFiltersViewDelegate.kt

--- app/src/main/java/com/company/activityart/presentation/edit_art_screen/subscreens/filters/EditArtFiltersViewDelegate.kt
+++ app/src/main/java/com/company/activityart/presentation/edit_art_screen/subscreens/filters/EditArtFiltersViewDelegate.kt
@@ -3,11 +3,8 @@ package com.company.activityart.presentation.edit_art_screen.subscreens.filters
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import com.company.activityart.presentation.edit_art_screen.subscreens.filters.EditArtFiltersViewState.*
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.company.activityart.architecture.EventReceiver
-import com.company.activityart.presentation.edit_art_screen.EditArtViewEvent
-import com.company.activityart.presentation.edit_art_screen.FilterStateWrapper
+import com.company.activityart.presentation.edit_art_screen.subscreens.filters.EditArtFiltersViewState.Loading
+import com.company.activityart.presentation.edit_art_screen.subscreens.filters.EditArtFiltersViewState.Standby
 
 @Composable
 fun EditArtFilters(viewModel: EditArtFiltersViewModel) {
