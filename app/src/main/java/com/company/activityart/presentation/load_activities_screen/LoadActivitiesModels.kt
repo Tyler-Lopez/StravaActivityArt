de9168339ef4a8c0817d0f5e59ceb7337bdc921b
Tyler Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2022-10-22 18:07:16 -0400

    Update LoadActivitiesModels.kt

--- app/src/main/java/com/company/activityart/presentation/load_activities_screen/LoadActivitiesModels.kt
+++ app/src/main/java/com/company/activityart/presentation/load_activities_screen/LoadActivitiesModels.kt
@@ -3,17 +3,17 @@ package com.company.activityart.presentation.load_activities_screen
 import com.company.activityart.architecture.ViewEvent
 import com.company.activityart.architecture.ViewState
 
-sealed class LoadActivitiesViewEvent : ViewEvent {
-    object ContinueClicked : LoadActivitiesViewEvent()
-    object TryAgainClicked : LoadActivitiesViewEvent()
-    object NavigateUpClicked : LoadActivitiesViewEvent()
+sealed interface LoadActivitiesViewEvent : ViewEvent {
+    object ContinueClicked : LoadActivitiesViewEvent
+    object TryAgainClicked : LoadActivitiesViewEvent
+    object NavigateUpClicked : LoadActivitiesViewEvent
 }
 
-sealed class LoadActivitiesViewState : ViewState {
+sealed interface LoadActivitiesViewState : ViewState {
     data class Loading(
         val totalActivitiesLoaded: Int = 0
-    ) : LoadActivitiesViewState()
+    ) : LoadActivitiesViewState
     data class LoadError(
         val totalActivitiesLoaded: Int = 0
-    ) : LoadActivitiesViewState()
+    ) : LoadActivitiesViewState
 }
