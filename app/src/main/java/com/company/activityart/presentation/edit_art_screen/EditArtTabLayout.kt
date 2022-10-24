f9334976802cf2a3d5614af9b4af172f60c273ae
Tyler Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2022-10-23 20:57:06 -0400

    Update EditArtTabLayout.kt

--- app/src/main/java/com/company/activityart/presentation/edit_art_screen/EditArtTabLayout.kt
+++ app/src/main/java/com/company/activityart/presentation/edit_art_screen/EditArtTabLayout.kt
@@ -2,19 +2,17 @@ package com.company.activityart.presentation.edit_art_screen
 
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.company.activityart.architecture.EventReceiver
-import com.company.activityart.presentation.edit_art_screen.EditArtViewEvent.*
+import com.company.activityart.presentation.edit_art_screen.EditArtViewEvent.PageHeaderClicked
 import com.company.activityart.presentation.ui.theme.Rust
 import com.company.activityart.presentation.ui.theme.White
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.pagerTabIndicatorOffset
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
