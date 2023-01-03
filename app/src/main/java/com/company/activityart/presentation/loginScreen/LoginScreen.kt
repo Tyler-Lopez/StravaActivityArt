4d20390eb742c506a6cf2b5dfbcbbfb78c41aea0
Tyler-Lopez <tylerlopezmi@gmail.com>
2023-01-02 20:11:29 -0500

    Login screen: update branding and copy

--- app/src/main/java/com/company/activityart/presentation/loginScreen/LoginScreen.kt
+++ app/src/main/java/com/company/activityart/presentation/loginScreen/LoginScreen.kt
@@ -2,11 +2,13 @@ package com.company.activityart.presentation.loginScreen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.company.activityart.R
 import com.company.activityart.presentation.common.type.Subhead
 import com.company.activityart.presentation.common.type.TitleOne
@@ -23,8 +25,11 @@ import com.company.activityart.presentation.editArtScreen.subscreens.filters.com
 fun LoginScreen(viewModel: LoginViewModel) {
     ScreenBackground {
         viewModel.viewState.collectAsState().value?.apply {
-            TitleOne(text = stringResource(id = R.string.app_name))
-            Subhead(text = stringResource(id = R.string.app_description))
+            Image(painterResource(id = R.drawable.ic_activity_art_logo), contentDescription = null)
+            Subhead(
+                text = stringResource(id = R.string.app_description),
+                modifier = Modifier.sizeIn(maxWidth = 254.dp)
+            )
             Image(
                 painter = painterResource(id = R.drawable.ic_btn_strava_connectwith_orange_clipped),
                 contentDescription = stringResource(id = R.string.connect_with_strava_button_cd),
