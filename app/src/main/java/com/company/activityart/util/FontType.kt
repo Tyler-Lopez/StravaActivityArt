7bf95083282a3e91739c41afc65f6fb0e4a9824b
Tyler-Lopez <tylerlopezmi@gmail.com>
2023-01-07 15:19:30 -0500

    Addition of Quicksand, Cinzel, Josefin, Satisfy fonts

--- app/src/main/java/com/company/activityart/util/FontType.kt
+++ app/src/main/java/com/company/activityart/util/FontType.kt
@@ -35,9 +35,49 @@ enum class FontType(
         fontName = R.string.font_newsreader_light,
         fileName = "newsreader_light"
     ),
+    CINZEL(
+        fontName = R.string.font_cinzel,
+        fileName = "cinzel_regular"
+    ),
+    SATISFY(
+        fontName = R.string.font_satisfy,
+        fileName = "satisfy_regular"
+    ),
     OSWALD(
         fontName = R.string.font_oswald,
         fileName = "oswald_regular"
+    ),
+    QUICKSAND_LIGHT(
+        fontName = R.string.font_quicksand_bold,
+        fileName = "quicksand_bold"
+    ),
+    QUICKSAND(
+        fontName = R.string.font_quicksand_regular,
+        fileName = "quicksand_regular"
+    ),
+    QUICKSAND_BOLD(
+        fontName = R.string.font_quicksand_light,
+        fileName = "quicksand_light"
+    ),
+    JOSEFIN_SANS_BOLD(
+        fontName = R.string.font_josefin_sans_bold,
+        fileName = "josefinsans_bold"
+    ),
+    JOSEFIN_SANS_BOLD_ITALIC(
+        fontName = R.string.font_josefin_sans_bold_italic,
+        fileName = "josefinsans_bolditalic"
+    ),
+    JOSEFIN_SANS_REGULAR(
+        fontName = R.string.font_josefin_sans_regular,
+        fileName = "josefinsans_regular"
+    ),
+    JOSEFIN_SANS_LIGHT(
+        fontName = R.string.font_josefin_sans_light,
+        fileName = "josefinsans_light"
+    ),
+    JOSEFIN_SANS_LIGHT_ITALIC(
+        fontName = R.string.font_josefin_sans_light_italic,
+        fileName = "josefinsans_lightitalic"
     );
 
     companion object {
@@ -47,6 +87,4 @@ enum class FontType(
 
     val assetFilepath: String
         get() = "$ASSET_FONTS_FOLDER_PREFIX$fileName$FONT_FILE_EXTENSION"
-
-
 }
