fd335614c07ab82067b55256f68a1aa7369cfd3b
Tyler-Lopez <tylerlopezmi@gmail.com>
2023-01-16 22:40:20 -0500

    Update images on login screen

--- app/src/main/java/com/activityartapp/presentation/loginScreen/LoginScreen.kt
+++ app/src/main/java/com/activityartapp/presentation/loginScreen/LoginScreen.kt
@@ -1,15 +1,19 @@
 package com.activityartapp.presentation.loginScreen
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -27,6 +31,99 @@ import com.activityartapp.presentation.ui.theme.StravaOrange
  */
 @Composable
 fun LoginScreen(viewModel: LoginViewModel) {
+    var orientation by remember { mutableStateOf(Configuration.ORIENTATION_PORTRAIT) }
+
+    val configuration = LocalConfiguration.current
+
+// If our configuration changes then this will launch a new coroutine scope for it
+    LaunchedEffect(configuration) {
+        // Save any changes to the orientation value on the configuration object
+        snapshotFlow { configuration.orientation }
+            .collect { orientation = it }
+    }
+
+    when (orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            LandscapeContent(viewModel)
+        }
+        else -> {
+            PortraitContent(viewModel)
+        }
+    }
+
+    /*
+    ScreenBackground(
+        padding = 0.dp,
+        scrollingEnabled = false
+    ) {
+        viewModel.viewState.collectAsState().value?.apply {
+
+            Image(painterResource(id = R.drawable.ic_activity_art_logo), contentDescription = null)
+            Subhead(
+                text = stringResource(id = R.string.app_description),
+                modifier = Modifier.sizeIn(maxWidth = 254.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.ic_btn_strava_connectwith_orange_clipped),
+                contentDescription = stringResource(id = R.string.connect_with_strava_button_cd),
+                modifier = Modifier
+                    .clickable { viewModel.onEvent(LoginViewEvent.ConnectWithStravaClicked) },
+            )
+        }
+    }
+
+     */
+}
+
+
+@Composable
+fun LandscapeContent(viewModel: LoginViewModel) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f, false)
+        ) {
+            Image(
+                painterResource(R.drawable.display_image),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize(),
+                alignment = Alignment.Center,
+                contentDescription = null
+            )
+        }
+        Spacer(modifier = Modifier
+            .fillMaxHeight()
+            .width(1.dp)
+            .background(StravaOrange))
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(painterResource(id = R.drawable.ic_activity_art_logo), contentDescription = null)
+            Subhead(
+                text = stringResource(id = R.string.app_description),
+                modifier = Modifier.sizeIn(maxWidth = 254.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.ic_btn_strava_connectwith_orange_clipped),
+                contentDescription = stringResource(id = R.string.connect_with_strava_button_cd),
+                modifier = Modifier
+                    .clickable { viewModel.onEvent(LoginViewEvent.ConnectWithStravaClicked) },
+            )
+        }
+    }
+}
+
+@Composable
+fun PortraitContent(viewModel: LoginViewModel) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -52,7 +149,7 @@ fun LoginScreen(viewModel: LoginViewModel) {
             modifier = Modifier
                 .padding(16.dp)
                 .fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
@@ -69,26 +166,4 @@ fun LoginScreen(viewModel: LoginViewModel) {
             )
         }
     }
-    /*
-    ScreenBackground(
-        padding = 0.dp,
-        scrollingEnabled = false
-    ) {
-        viewModel.viewState.collectAsState().value?.apply {
-
-            Image(painterResource(id = R.drawable.ic_activity_art_logo), contentDescription = null)
-            Subhead(
-                text = stringResource(id = R.string.app_description),
-                modifier = Modifier.sizeIn(maxWidth = 254.dp)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.ic_btn_strava_connectwith_orange_clipped),
-                contentDescription = stringResource(id = R.string.connect_with_strava_button_cd),
-                modifier = Modifier
-                    .clickable { viewModel.onEvent(LoginViewEvent.ConnectWithStravaClicked) },
-            )
-        }
-    }
-
-     */
 }
