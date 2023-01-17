5bac5aa1365cface683bca5c67cdf61e60375daf
Tyler-Lopez <tylerlopezmi@gmail.com>
2023-01-16 20:22:04 -0500

    Add picture to the login screen

--- app/src/main/java/com/activityartapp/presentation/loginScreen/LoginScreen.kt
+++ app/src/main/java/com/activityartapp/presentation/loginScreen/LoginScreen.kt
@@ -1,17 +1,22 @@
 package com.activityartapp.presentation.loginScreen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.activityartapp.R
 import com.activityartapp.presentation.common.type.Subhead
 import com.activityartapp.presentation.common.ScreenBackground
+import com.activityartapp.presentation.ui.theme.StravaOrange
 
 /**
  * When the athlete is determined as unauthenticated,
@@ -22,8 +27,35 @@ import com.activityartapp.presentation.common.ScreenBackground
  */
 @Composable
 fun LoginScreen(viewModel: LoginViewModel) {
-    ScreenBackground {
-        viewModel.viewState.collectAsState().value?.apply {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f, false)
+        ) {
+            Image(
+                painterResource(R.drawable.display_image),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize(),
+                alignment = Alignment.BottomCenter,
+                contentDescription = null
+            )
+        }
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(StravaOrange))
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
             Image(painterResource(id = R.drawable.ic_activity_art_logo), contentDescription = null)
             Subhead(
                 text = stringResource(id = R.string.app_description),
@@ -37,4 +69,26 @@ fun LoginScreen(viewModel: LoginViewModel) {
             )
         }
     }
+    /*
+    ScreenBackground(
+        padding = 0.dp,
+        scrollingEnabled = false
+    ) {
+        viewModel.viewState.collectAsState().value?.apply {
+
+            Image(painterResource(id = R.drawable.ic_activity_art_logo), contentDescription = null)
+            Subhead(
+                text = stringResource(id = R.string.app_description),
+                modifier = Modifier.sizeIn(maxWidth = 254.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.ic_btn_strava_connectwith_orange_clipped),
+                contentDescription = stringResource(id = R.string.connect_with_strava_button_cd),
+                modifier = Modifier
+                    .clickable { viewModel.onEvent(LoginViewEvent.ConnectWithStravaClicked) },
+            )
+        }
+    }
+
+     */
 }
--- app/src/main/java/com/activityartapp/presentation/welcomeScreen/WelcomeScreen.kt
+++ app/src/main/java/com/activityartapp/presentation/welcomeScreen/WelcomeScreen.kt
@@ -1,5 +1,6 @@
 package com.activityartapp.presentation.welcomeScreen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.CircularProgressIndicator
@@ -7,6 +8,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
