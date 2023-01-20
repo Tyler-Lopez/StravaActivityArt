83bbf668f0e4cfbf02b011995588a8863ee4fb45
Tyler-Lopez <tylerlopezmi@gmail.com>
2023-01-19 23:35:30 -0500

    Fix bug with quirk in permissions

--- app/src/main/java/com/activityartapp/presentation/saveArtScreen/SaveArtStandby.kt
+++ app/src/main/java/com/activityartapp/presentation/saveArtScreen/SaveArtStandby.kt
@@ -1,14 +1,16 @@
 package com.activityartapp.presentation.saveArtScreen
 
 import android.Manifest
-import android.content.Intent.*
 import android.graphics.Bitmap
 import android.os.Build
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.SnackbarHost
+import androidx.compose.material.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,19 +18,18 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
-import com.activityartapp.presentation.saveArtScreen.SaveArtViewEvent.*
-import com.activityartapp.BuildConfig
 import com.activityartapp.R
 import com.activityartapp.architecture.EventReceiver
 import com.activityartapp.presentation.common.ScreenBackground
 import com.activityartapp.presentation.common.button.ButtonSize
 import com.activityartapp.presentation.common.button.HighEmphasisButton
 import com.activityartapp.presentation.common.button.MediumEmphasisButton
+import com.activityartapp.presentation.saveArtScreen.SaveArtViewEvent.*
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.rememberPermissionState
 
 
@@ -42,9 +43,28 @@ fun SaveArtStandby(
     eventReceiver: EventReceiver<SaveArtViewEvent>,
     snackbarHostState: SnackbarHostState
 ) {
+
     /** OS Greater than Android 10 (API 30) do not require permissions **/
     val osGreaterThan10 = Build.VERSION.SDK_INT > Build.VERSION_CODES.Q
     val permissionState = rememberPermissionState(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+    /** Unfortunate necessary hack with the permissions API.
+     * See the section "Limitations" at
+     * @link(https://google.github.io/accompanist/permissions)
+     *
+     * Because there is no way to differentiate before the first invocation of
+     * [PermissionState.launchPermissionRequest] whether the first permission request
+     * will not be successful due to the user having previously clicked "Do Not Ask Me Again".
+     *
+     * To get around this, we ask immediately rather than after Download is pressed. When
+     * they click "Download" thereafter we know whether or not they are perma-denied. **/
+    SideEffect {
+        permissionState.apply {
+            if (!permissionRequested || (permissionRequested && shouldShowRationale)) {
+                launchPermissionRequest()
+            }
+        }
+    }
 
     /** Activity resumes upon returning to foreground when share activity is closed **/
     OnLifecycleEvent { _, event ->
@@ -74,14 +94,12 @@ fun SaveArtStandby(
                             osGreaterThan10 || hasPermission -> {
                                 eventReceiver.onEventDebounced(ClickedDownload)
                             }
-                            shouldShowRationale -> {
-                                permissionState.launchPermissionRequest()
+                            !permissionRequested || (permissionRequested && shouldShowRationale) -> {
+                                launchPermissionRequest()
                             }
-                            else -> {
-                                eventReceiver.onEventDebounced(
-                                    ClickedDownloadWhenPermissionPermaDenied
-                                )
-                            }
+                            else -> eventReceiver.onEventDebounced(
+                                ClickedDownloadWhenPermissionPermaDenied
+                            )
                         }
                     }
                 }
