5dcb6e087730cf8e12e5bf2d8fbd2af4b603d5f7
Tyler Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2023-02-14 20:08:05 -0500

    Update logo (#24)

--- app/src/main/java/com/activityartapp/presentation/AppLogo.kt
+++ app/src/main/java/com/activityartapp/presentation/AppLogo.kt
@@ -1,15 +1,29 @@
 package com.activityartapp.presentation
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.requiredSizeIn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.activityartapp.R
+import com.activityartapp.presentation.common.layout.ColumnSmallSpacing
 
 @Composable
 fun AppLogo() {
-    Image(
-        painter = painterResource(id = R.drawable.ic_activity_art_logo),
-        contentDescription = stringResource(id = R.string.app_logo_content_description)
-    )
+    ColumnSmallSpacing {
+        Image(
+            painter = painterResource(id = R.drawable.ic_logo),
+            modifier = Modifier.requiredSizeIn(maxWidth = 64.dp),
+            contentDescription = stringResource(id = R.string.app_logo_content_description)
+        )
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.h5
+        )
+    }
 }
