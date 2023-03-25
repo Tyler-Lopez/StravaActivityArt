dad2204cbeeac56dbed9ab1857b1cdb1a9e6a069
Tyler-Lopez <tylerlopezmi@gmail.com>
2023-03-25 18:02:26 -0400

    Add night mode to the application

--- app/src/main/java/com/activityartapp/presentation/MainActivity.kt
+++ app/src/main/java/com/activityartapp/presentation/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
